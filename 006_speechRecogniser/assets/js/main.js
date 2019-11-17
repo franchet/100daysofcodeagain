cadf343a983d579ba26123ab9d9c980ab10016b8
Franchet <franchetthomas@gmail.com>
2019-11-17 07:38:53 -0500

    updated js file

---
+++ 006_speechRecogniser/assets/js/main.js
@@ -0,0 +1,59 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+const transcript_element = document.getElementById("transcript");
+const talk_button = document.getElementById("start");
+const end_button = document.getElementById("end");
+
+let p = document.createElement("p");
+transcript_element.appendChild(p);
+
+recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join("");
+
+    if (transcript.startsWith("Gideon")) {
+        p.textContent = transcript;
+        if (e.results[0].isFinal) {
+            p = document.createElement("p");
+            p.textContent = transcript;
+            transcript_element.appendChild(p);
+            p.textContent = "";
+
+            if (transcript.includes("weather")) {
+                console.log("Weather");
+
+                let command = document.createElement("p");
+                command.classList.add("command");
+                command.textContent = "Getting Weather...";
+
+                transcript_element.appendChild(command);
+            }
+        }
+    }
+});
+
+recognition.addEventListener("end", recognition.start);
+
+recognition.start();
+// recognition.addEventListener("end", () => {
+// 	end_button.disabled = true;
+// 	talk_button.disabled = false;
+// });
+
+// talk_button.addEventListener("click", () => {
+// 	end_button.disabled = false;
+// 	talk_button.disabled = true;
+
+// 	recognition.start();
+// });
+
+// end_button.addEventListener("click", () => {
+// 	end_button.disabled = true;
+// 	talk_button.disabled = false;
+
+// 	recognition.stop();
+// });
