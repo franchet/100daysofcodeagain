c5a9839140b23c9d8c30da4917c661fa86efe863
Franchet <franchetthomas@gmail.com>
2019-11-25 08:21:00 -0500

    updated

---
+++ 012_VueToDoList/vue-todo/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+	<div id="app">
+		<Header />
+		<Todos :todos="todos" />
+	</div>
+</template>
+
+<script>
+	import Header from './components/Header.vue';
+	import Todos from './components/Todos.vue'
+
+
+	let todos = [
+		{
+			id:"1",
+			name: "Record Video",
+			done: "false"
+			},
+			{
+				id: "2",
+				name: "Upload Video",
+				done: "false"
+				},
+				{
+					id: "3",
+					name: "Party...",
+					done: "false"
+					},
+				{
+					id: "4",
+					name: "Debug",
+					done: "false"
+				},
+				{
+					id: "5",
+					name: "More Debug",
+					done: "false"
+				}
+					];
+
+
+export default {
+	name: 'app',
+	components: {
+		Header, Todos
+	},
+	data: function () {
+		return {
+			todos: todos
+		}
+	}
+}
+
+</script>
+
+
+<style lang="scss">
+body {
+	margin: 0;
+	padding: 0;
+	background-color: #f3f3f3;
+
+	font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ 012_VueToDoList/vue-todo/src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <header>
+        <input type="text" name="todoInput" id="todoInput" placeholder="What's the plan, Batman?">
+        <button class="submit" v-on:click="$emit(createTodo, document.getElementById('todoInput').value)">+</button>
+    </header>
+</template> 
+
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+
+<style lang="scss" scoped>
+    header {
+        display: flex;
+
+        input {
+            flex: 1;
+            height: 50px;
+            box-sizing: border-box;
+            text-indent: 15px;
+
+            font-size: 20px;
+        }
+
+        .submit {
+            width: 50px;
+            height: 50px;
+
+            color: #171717;
+            font-size: 28px;
+            line-height: 1;
+            font-weight: 700;
+
+
+
+            background-color: #ffce00;
+            border: none;
+            outline: none;
+
+            cursor: pointer;
+
+            &:hover {
+                background-color: darken($color: #ffce00, $amount: 10);
+            }
+
+            &:active {
+                background-color: darken($color: #ffce00, $amount: 5);;
+            }
+        }
+    }
+</style>
