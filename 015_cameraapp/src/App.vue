0b7474899344c81060df472dc88e92329f1c2d57
Franchet <franchetthomas@gmail.com>
2019-11-27 20:49:44 -0500

    updated

--- 015_cameraapp/src/App.vue
+++ 015_cameraapp/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Camera />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Camera from './components/Camera'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		Camera
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+#body {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	margin: 0;
+	padding: 0;
+	background-color: f3f3f3;
+
 }
 </style>
---
+++ 015_cameraapp/src/components/Camera.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="camera">
+        <video autoplay class="feed"></video>
+        <button class="snap">SNAP</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "app"
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.camera {
+    width: 100vw;
+    height: 100vh;
+
+    .feed {
+        display: block;
+        width: 100%;
+        max-width: 1280px;
+
+        margin: 0 auto;
+
+        box-shadow: 4px 4px 12px 0px rgba(0, 0, 0, 0.25);
+        }
+}
+</style>
